44416a68bb6d9a16e0d8544e5d98d66ae0f93dac
Luis Laens <lulaens@1E5.42angouleme.fr>
2023-10-04 16:18:04 +0200

    Topic commentaire

--- Topic.cpp
+++ Topic.cpp
@@ -39,13 +39,13 @@ void    Server::Topic(Client *client, String entry) {
 
     String topic = entry.substr(i, entry.find('\0', name.size()));
     
-    /*  verifier les droit    */
-    /* faire attention au index_chan */
-
+    /*  Si le channel existe    */
     if (IfChannelExist(name) == true) {
         int index_chan = Utils::findChannelIndex(name, channelList);
  
+        /*  Si le topic rentré en param est vide (String topic au dessus) */
         if (topic.empty()) {
+            /*  Si le topic enregistré (la variable topic dans la classe) est vide aussi envoyer RPL_NOTOPIC */
             if (this->channelList[index_chan]->getTopic().empty()) {
                 sendMsg(RPL_NOTOPIC(client->getNickname(), this->channelList[index_chan]->getName()), client->getFd(), client->getNickname());
                 return ; 
@@ -53,16 +53,22 @@ void    Server::Topic(Client *client, String entry) {
         }
         if (!topic.empty())
         {
+            /*  Si le topic n'est pas vide on verifie si il a les droit avec des check du fd owner et admin
+                Ex: /TOPIC #chan disussion */
             if (this->channelList[index_chan]->getOwner() != client->getFd()) {
                 if (this->channelList[index_chan]->FdIsAdmin(client->getFd()) == false) {
                     sendMsg(ERR_CHANOPRIVSNEEDED(client->getNickname(), this->channelList[index_chan]->getName()), client->getFd(), client->getNickname());
                     return ;
                 }
             }
+            /*  On envoie la String topic crée au dessus    */
             this->channelList[index_chan]->setTopic(topic);
             sendMsg(RPL_TOPIC(client->getNickname(), this->channelList[index_chan]->getName(), topic), client->getFd(), client->getNickname());
         }
         else {
+            /*  Et sinon donc si le topic est vide et que la variable topic de la classe est remplie,
+                on l'envoie, pour que le client renvoie le nom du topic actuel.
+                Ex: /TOPIC #chan1 */
             String topic_chan = this->channelList[index_chan]->getTopic();
             sendMsg(RPL_TOPIC(client->getNickname(), this->channelList[index_chan]->getName(), topic_chan), client->getFd(), client->getNickname());
         }
