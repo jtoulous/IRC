85a696a15950c31af25a7400e56dda2273227414
josh <toulouse.joshua97@gmail.com>
2023-10-06 05:54:11 -0400

    mode

--- Mode.cpp
+++ Mode.cpp
@@ -1,28 +1,28 @@
 #include "Server.hpp"                                                      
 
-static void execMode(String mode, Channel *channel, Client *Target, Client *Admin)
-{                                                                            
-  if (mode == '+i' || mode == '-i')                                          
-    iMod();///////////////////////////////////////////////////              
-  else if (mode == '+t' || mode == '-t')                               
-    tMod();///////////////////////////////////////////////////             
-  else if (mode == '+k' || mode == '-k')                                 
-    kMod();///////////////////////////////////////////////////          
-  else if (mode == '+o' || mode == '-o')                               
-    oMod();//////////////////////////////////////////////////          
-  else if (mode == '+l' || mode == '-l')                                
-    lMod();//////////////////////////////////////////////////
-  else
-    sendMsg(ERR_NOTVALIDMOD(), Admin->getFd(), Admin->getNickname());
-}
+//static void execMode(String mode, Channel *channel, Client *Target, Client *Admin)
+//{                                                                            
+//  if (mode == '+i' || mode == '-i')                                          
+//    iMod();///////////////////////////////////////////////////              
+//  else if (mode == '+t' || mode == '-t')                               
+//    tMod();///////////////////////////////////////////////////             
+//  else if (mode == '+k' || mode == '-k')                                 
+//    kMod();///////////////////////////////////////////////////          
+//  else if (mode == '+o' || mode == '-o')                               
+//    oMod();//////////////////////////////////////////////////          
+//  else if (mode == '+l' || mode == '-l')                                
+//    lMod();//////////////////////////////////////////////////
+//  else
+//    sendMsg(ERR_NOTVALIDMOD(), Admin->getFd(), Admin->getNickname());
+//}
 
-static void  parseModes(vector<String> &modes, String entry)
+static void  parseModes(vector<String> &modes, String entry, String client)
 {
   char    operator = entry.wordStartChar(1);
   String  mod;
 
   if (operator != '-' && operator != '+')
-        throw (Xception());//bad input
+        throw (Xception(ERR_UNKNOWNCOMMAND(client, entry)));//bad input
   
   for (int i = 1; i <= entry.wordCount(); i++)
   {
@@ -31,7 +31,7 @@ static void  parseModes(vector<String> &modes, String entry)
     for (int j = 0; word[j]; j++)
     {
       if (!Utils::modValidChar(word[j]))
-        throw (Xception());//bad input
+        throw (Xception(ERR_UNKNOWNCOMMAND(client, entry)));//bad input
       
       if (word[j] == '+' || word[j] == '-')
         operator = word[j];
@@ -56,22 +56,22 @@ void  Server::mode(Client *client, String &entry)
   try
   {
     if (channelIndex == -1)//channel existe pas
-      throw (Xception(ERR_BLABLA));
+      throw (Xception(ERR_NOSUCHCHANNEL(client->getNickname())));
     
     if (!channelList[channelIndex]->FdIsAdmin(client->getFd()))//pas admin
-      throw (Xception(ERR_BLABLA));
+      throw (Xception(ERR_CHANOPRIVSNEEDED(client->getNickname(), channelList[channelIndex]->getName())));
 
     if (entry.wordStartChar(1) != '+' && entry.wordStartChar(1) != '-')
     { 
       fdTarget = Utils::findClientFd(entry.extractWord(1), clientList);
       if (fdTarget == -1)//le client cible existe pas
-        throw (Xception(ERR_BLABLA));
+        throw (Xception(ERR_NOSUCHNICK(client->getNickname())));
     }
 
-    parseModes(modes, entry)//mode invalid
+    parseModes(modes, entry, client->getNickname())//mode invalid
 
-    for (int i = 0; i < (int)modes.size(); i++)//execution 1 par 1
-      execMode(modes[i], channelList[channelIndex], clientList[Utils::findClientIndex(fdTarget)], client);
+    //for (int i = 0; i < (int)modes.size(); i++)//execution 1 par 1
+    //  execMode(modes[i], channelList[channelIndex], clientList[Utils::findClientIndex(fdTarget)], client);
   }
 
   catch (Xception &e)
--- Utils.hpp
+++ Utils.hpp
@@ -23,7 +23,8 @@ class Utils
             static bool         nicknameAvailable(String &nickname, vector<Client *> &clientList);
             static bool         usernameAvailable(String &username, vector<Client *> &clientList);
             //static void         rmFromServer(Client *client, vector<Client *> &clientList, vector<Channel *> &channelList);
-            static int         passSpace(String str);
+            static int          passSpace(String str);
+            bool                modValidChar(char ch);
     private:
             Utils();
             //Utils(const Utils &ref);
